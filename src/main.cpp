e5596c98493afc189cbb50c11566eea2c0493e3b
shachafk <katz.shachaf@gmail.com>
2019-11-18 20:49:31 +0200

    test for parsing json

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "../include/json.hpp"
 #include "../include/Session.h"
 using namespace std;
 
 int main(int argc, char** argv){
-  //  std::cout << "Hello, World!" << std::endl;
+   //std::cout << "Hello, World!" << std::endl;
 
 
+    std::ifstream i("../config1.json");
+    nlohmann::json j;
+    i >> j;
+    //  std::cout<<j;
+    std::cout<<j["tv_series"];
+    //std::cout <<std::setw(4) << j << std::endl;
+
+
+    //std::vector<std::uint8_t> v = {'t', 'r', 'u', 'e'};
+    //nlohmann::json j = nlohmann::json::parse(v.begin(), v.end());
+
+
+
+/*
     if(argc!=2)
     {
         cout << "usage splflix input_file" << endl;
@@ -14,5 +31,5 @@ int main(int argc, char** argv){
     Session s(argv[1]);
     s.start();
     return 0;
-
+*/
 }
