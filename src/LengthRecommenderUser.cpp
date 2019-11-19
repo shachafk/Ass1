0d2e2ef15ecad6e2b9b5cd9a16b606bead499d7d
amitmichael <amitmichael22@gmail.com>
2019-11-19 22:06:19 +0200

    19/11/19

---
+++ src/LengthRecommenderUser.cpp
@@ -0,0 +1,8 @@
+//
+// Created by amitmich@wincs.cs.bgu.ac.il on 19/11/2019.
+//
+#include "../include/User.h"
+LengthRecommenderUser(const std::string& name):name(name){};
+ Watchable* getRecommendation(Session& s){
+     return
+ }
