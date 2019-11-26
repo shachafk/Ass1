1ebe78166cbbd30a6b2867cbc5fa4d5c62a8323d
amitmichael <amitmichael22@gmail.com>
2019-11-26 05:52:55 -0800

    enee

--- src/GenreRecommenderUser.cpp
+++ src/GenreRecommenderUser.cpp
@@ -12,5 +12,38 @@ User* GenreRecommenderUser::clone(std::string name_) {
     other->copyAvailable(getAvailable());
     return other;
 }
- Watchable* GenreRecommenderUser::getRecommendation(Session& s){return nullptr;}
+ Watchable* GenreRecommenderUser::getRecommendation(Session& s){
+ /*    std::vector<Watchable *> hist = get_history();//get user's history vector
+     std::map<long, Watchable*>* avilable =  getAvailable();
+     std::vector<std::string,int> sortedTags;
+     int maxcount=0;
+     std::string temp;
+     std::vector<Watchable *>::iterator it;
+     for(it = hist.begin();it!=hist.end();it++) { //go over the user's history
+         const std::vector<std::string> &tags = (*it).getTag(); //check it TBD
+         for (int i = 0; i < tags.size(); i++) { //go over each tag
+             std::string curr = tags.at(i);
+             std::vector<int>::iterator it = std::find(sortedTags.begin(), sortedTags.end(), curr)
+             if (it != sortedTags.end()) {
+                 it->second++;
+             } else {
+                 sortedTags.push_back(std::make_pair(curr, 1));
+             }
+         }
+     }
+
+
+     /*        for (int i = 0; i < getTag().size; i++)
+                 for (int j = i + 1; j < getTag().size; j++) {
+                     if (tags.at(i) > tags.at(j)) {
+                         temp = tags.at(i);
+                         tags.at(i) = tags.at(j);
+                         tags.at(j) = temp;
+                     }
+*/
+
+
+      //       }
+
+     return nullptr;}
 
